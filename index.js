1c427d815ae6aa6baa49bf4f4cd86134ef7ddb22
Jordan Harband <ljharb@gmail.com>
2020-08-29 14:36:48 -0700

    Github Actions does not support node 14 yet

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import core from '@actions/core';
-import github from '@actions/github';
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 const token = core.getInput('github_token') || process.env.GITHUB_TOKEN;
 
@@ -7,17 +7,15 @@ const octokit = github.getOctokit(token);
 
 const pull_number = parseInt(core.getInput('pull_number'), 10);
 
-try {
-	const { data } = await octokit.pulls.get({
-		...github.context.repo,
-		pull_number,
-	});
-
+octokit.pulls.get({
+	...github.context.repo,
+	pull_number,
+}).then(({ data }) => {
 	core.setOutput('maintainer_can_modify', data.maintainer_can_modify);
 
 	if (!data.maintainer_can_modify) {
 		core.setFailed('This pull request must have the “allow edits” checkbox checked.');
 	}
-} catch (error) {
-	core.setFailed(error ?? 'Unknown error');
-}
+}).catch((error) => {
+	core.setFailed(error || 'Unknown error');
+});
